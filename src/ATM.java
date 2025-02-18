e4f173a5059cc858d56b88ab7d8c87a5ec35d46f
rayush0325 <rayush2504@gmail.com>
2025-02-18 17:29:52 +0530

    implemeted things by making functions non-static

--- src/ATM.java
+++ src/ATM.java
@@ -2,37 +2,37 @@ import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.Scanner;
 
 public class ATM {
-    private static int[][] accounts = {
-            {1234, 50000},
-            {2345, 60000},
-            {3456, 70000},
-            {4567, 80000},
-            {5678, 90000},
-            {6789, 1000}
-    };
+    private int balance = 50000;
+    private int PIN = 4567;
     public static void main(String[] args) {
+        ATM atm = new ATM();
         Scanner scanner = new Scanner(System.in);
         int PIN;
         while(true){
             System.out.println("Enter your PIN");
             PIN = scanner.nextInt();
-            if(isValid(PIN)){
+            if(atm.isValid(PIN)){
                 break;
             }
         }
+        atm.menu();
+    }
+    private void  menu(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("WELCOME");
         while (true){
             System.out.println(
-                            "1. Check Balance\n" +
-                            "2. Deposit Money\n" +
-                            "3. Withdraw Money\n" +
-                            "4. Exit\n"+
-                            "choose option [1 ... 4]"
+                    "1. Check Balance\n" +
+                    "2. Deposit Money\n" +
+                    "3. Withdraw Money\n" +
+                    "4. Exit\n"+
+                    "choose option [1 ... 4]"
             );
             int option = scanner.nextInt();
             if(option == 4){
                 //exit
                 System.out.println("Thank you for visiting using our ATM");
+                scanner.close();
                 break;
             }
             switch (option){
@@ -49,46 +49,28 @@ public class ATM {
         }
 
     }
-    private static boolean isValid(int PIN){
+    private boolean isValid(int PIN){
         if(PIN < 1000 || PIN > 9999){
             System.out.println("Enter 4 digit PIN");
             return false;
         }
-        if(!isPresent(PIN)){
-            System.out.println("PIN incorrect");
-            return false;
+        if(this.PIN == PIN){
+            return true;
         }
-        return true;
-    }
-    private static boolean isPresent(int PIN){
-        for(int[] account : accounts){
-            if(account[0] == PIN){
-                return true;
-            }
-        }
+        System.out.println("incorrect PIN");
         return false;
     }
-    private static void checkBalance(int PIN){
-        for(int[] account : accounts){
-            if(account[0] == PIN){
-                System.out.println("your balance is "+account[1]);
-            }
-        }
+    private void checkBalance(int PIN){
+        System.out.println("your balance is "+this.balance);
     }
-    private static void depositMoney(int PIN){
+    private void depositMoney(int PIN){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter amount");
         int amount = scanner.nextInt();
-        for(int[] account : accounts){
-            if(account[0] == PIN){
-                account[1] += amount;
-                System.out.println("Amount deposited successfully");
-                return;
-            }
-        }
-        System.out.println("Some error occurred");
+        this.balance += amount;
+        System.out.println("Amount deposited successfully");
     }
-    private static void withdrawMoney(int PIN){
+    private void withdrawMoney(int PIN){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter amount");
         int amount = scanner.nextInt();
@@ -96,17 +78,11 @@ public class ATM {
             System.out.println("Amount > 2000 can't be withdrawn");
             return;
         }
-        for(int[] account : accounts){
-            if(account[0] == PIN){
-                if(account[1] < amount){
-                    System.out.println("Insufficient balance");
-                    return;
-                }
-                account[1] -= amount;
-                System.out.println("Amount withdrawn successfully");
-                return;
-            }
+        if(this.balance < amount){
+            System.out.println("Insufficient balance");
+            return;
         }
-        System.out.println("Some error occurred");
+        this.balance -= amount;
+        System.out.println("Amount withdrawn successfully");
     }
 }
